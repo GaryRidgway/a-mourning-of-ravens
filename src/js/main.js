9aab197303c71c85e2c928319aab22bf0805a1a6
Alan Way <gary-ridgway@uiowa.edu>
2024-05-14 08:35:07 -0500

    Removal of extraneous code.

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const startStanzaIndex = randomStanzaIndex();
   const startStanza = fetchStagedStanza(startStanzaIndex);
-  const anchorStanza = placeFirstStanza(startStanza);
+  placeFirstStanza(startStanza);
 
   cascadeRender();
 });
